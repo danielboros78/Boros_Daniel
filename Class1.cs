1451ac2f99454a0841c72d591d920fefddfdbb08
danielboros78 <danielboros78@gmail.com>
2021-09-17 08:08:06 +0200

    Kiindulo helyzet

---
+++ Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boros_Daniel
+{
+  class Adatok
+  {
+    private string nev;
+
+    public string Nev
+    {
+      get { return nev; }
+      set { nev = value; }
+    }
+
+    private string nem;
+
+    public string Nem
+    {
+      get { return nem; }
+      set { nem = value; }
+    }
+
+    private string reszleg;
+
+    public string Reszleg
+    {
+      get { return reszleg; }
+      set { reszleg = value; }
+    }
+
+    private int ev;
+
+    public int Ev
+    {
+      get { return ev; }
+      set { ev = value; }
+    }
+
+    private int fizetes;
+
+    public int Fizetes
+    {
+      get { return fizetes; }
+      set { fizetes = value; }
+    }
+
+    public Adatok(string nev, string nem, string reszleg, int ev, int fizetes)
+    {
+      this.nev = nev;
+      this.nem = nem;
+      this.reszleg = reszleg;
+      this.ev = ev;
+      this.fizetes = fizetes;
+    }
+  }
+}
--- Program.cs
+++ Program.cs
@@ -8,7 +8,49 @@ namespace Boros_Daniel
 {
   class Program
   {
+    static List<Adatok> adatok = new List<Adatok>();
     static void Main(string[] args)
+    {
+      ElsoFeladat();
+      MasodikFeladat();
+      HarmadikFeladat();
+      NegyedikFeladat();
+      OtodikFeladat();
+      HatodikFeladat();
+      HetedikFeladat();
+    }
+
+    private static void HetedikFeladat()
+    {
+
+    }
+
+    private static void HatodikFeladat()
+    {
+
+    }
+
+    private static void OtodikFeladat()
+    {
+
+    }
+
+    private static void NegyedikFeladat()
+    {
+
+    }
+
+    private static void HarmadikFeladat()
+    {
+
+    }
+
+    private static void MasodikFeladat()
+    {
+
+    }
+
+    private static void ElsoFeladat()
     {
 
     }
